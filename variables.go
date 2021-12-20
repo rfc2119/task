5da84c0717c3501a714793911c41d3091a167890
rfc2119 <rfc2119@users.noreply.github.com>
2021-12-20 09:30:00 +0000

    feature: hide tasks when listing with `task -l`

--- variables.go
+++ variables.go
@@ -61,6 +61,7 @@ func (e *Executor) compiledTask(call taskfile.Call, evaluateShVars bool) (*taskf
 		Prefix:      r.Replace(origTask.Prefix),
 		IgnoreError: origTask.IgnoreError,
 		Run:         r.Replace(origTask.Run),
+		Hide:        origTask.Hide,
 	}
 	new.Dir, err = execext.Expand(new.Dir)
 	if err != nil {
